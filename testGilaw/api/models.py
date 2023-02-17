28be36893a4078f6588c6b048f242bad6e5c5d26
yasser <yazanenator@gmail.com>
2023-02-17 05:21:57 -0600

    change the models for resolve enum

--- testGilaw/api/models.py
+++ testGilaw/api/models.py
@@ -15,13 +15,12 @@ class Notification(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
 
-
     class Meta:
         db_table = "notification"
 
     def __str__(self):
         return self.message
-    
+
 
 class SMSNotification(models.Model):
     message = models.TextField(null=False, verbose_name="message")
@@ -39,7 +38,7 @@ class EmailNotification(models.Model):
 
     def __str__(self):
         return self.message
-    
+
 
 class PushNotification(models.Model):
     message = models.TextField(null=False, verbose_name="message")
@@ -48,21 +47,25 @@ class PushNotification(models.Model):
 
     def __str__(self):
         return self.message
-    
-    
+
+
 class UserNotification(models.Model):
     class Category(Enum):
-        S = "Sports"
-        F = "Finance"
-        M = "Movies"
+        sports = ('Sports', 'Sports')
+        finance = ('Finance', 'Finance ')
+        movies = ('Movies', 'Movies')
 
         @classmethod
         def choices(cls):
             return [(item.value, item.name) for item in cls]
-    
+
+        @classmethod
+        def get_value(cls, member):
+            return cls[member].value[0]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=250, null=False,
                             blank=True, verbose_name="name")
     phone = models.IntegerField(null=False, blank=True, verbose_name="Phone")
     channels = models.ForeignKey(Notification, on_delete=models.CASCADE)
-    subscribed =  models.CharField(max_length=2, choices=[x.value for x in Category])
+    subscribed = models.CharField(max_length=50, choices=[x.value for x in Category], default=Category.get_value('sports'),)
